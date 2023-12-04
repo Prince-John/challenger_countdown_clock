57fa0feac8bfc7d6af7ca6180a95b7d81d9b8a6a
hvnguyen4 <h.v.nguyen@wustl.edu>
2023-12-04 10:37:16 -0600

    Update CountdownClock.py
    Countdown Clock For Adafruit MatrixPortal M4 with use of up and down buttons

--- CountdownClock/CountdownClock.py
+++ CountdownClock/CountdownClock.py
@@ -1,44 +1,172 @@
-import time,board,displayio
-from adafruit_matrixportal.keypad import Keypad
+import time
+import board
+import digitalio
+import rainbowio
+import terminalio
+from adafruit_display_text import bitmap_label
 from adafruit_matrixportal.matrixportal import MatrixPortal
 
-# Pins for the LED panel
-CLK=board.D11
-OE=board.D9
-LAT=board.D10
-A=board.A0
-B=board.A1
-C=board.A2
-D=board.A3
+# Handles setting the timer
+def settingTimer(): 
+    global minutes,seconds,timerState
 
-# Initialize matrix setup. "double_buffer" enables double-buffering, which keeps the animation from flickering
-# The "64" is there because this is a 32x64 matrix
-matrix=MatrixPortal(
-    width=64,
-    height=32,
-    gpio=displayio.FourWire(CLK,LAT,A,B,C,D),
-)
+    digits=[0,0,0,0] # Array to store the digits of the timer
+    counterNum=0 # Counter to keep track of the number of times the up button is pressed
+    downButtonHits=0 # Counter to keep track of the number of times the down button is pressed
+    while(True):
+        minutes=digits[3]*10+digits[2]
+        seconds=digits[1]*10+digits[0]
+        timeStr="{:02d}:{:02d}".format(minutes,seconds)
 
-# Setting up text
-str="BLASTOFF"  # Scrolling text string
-textX=matrix.display.width  # Width of the matrix for text string
-textMin=len(str)*-12  # Setting number of columns at the right side after the last letter before we reset the scrolling pattern
-hue=0   # Hue to start at zero
+        showTime(timeStr,1)
 
-# Defining our pins for the keypad
-numROWS=4
-numCOLS=4
-keys=[
-    ['1','2','3','A'],
-    ['4','5','6','B'],
-    ['7','8','9','C'],
-    ['*','0','#','D']
-]
-rowPins=[board.D40,board.D41,board.D42,board.D43]
-colPins=[board.D44,board.D45,board.D46,board.D47]
+        # If the up button is pressed, increment the counterNum by 1
+        if(not upButton.value):
+            time.sleep(0.3)
 
-# Initialize keypad
-keypad=Keypad.make_keymap(keys,rowPins,colPins,numROWS,numCOLS)
+            counterNum=(counterNum+1)%10 # Increments the counterNum by 1 and wraps around to 0 if it reaches 10
+            digits[downButtonHits]=counterNum # Sets the digit at the downButtonHits index to the counterNum
+
+            minutes=digits[3]*10+digits[2]
+            seconds=digits[1]*10+digits[0]
+        # If the down button is pressed, move to the next digit
+        elif(not downButton.value):
+            time.sleep(0.3)
+
+            counterNum=0
+            downButtonHits+=1
+
+            # If the downButtonHits counter is 4, all four digits are set and the timer is ready to run
+            if(downButtonHits==4):
+                # If time is greater than 99:60, set it to 99:60
+                if(minutes==99 and seconds>=60):
+                    seconds=59
+                timerState=TimerState.RUNNING
+                break
+
+# Handles running the timer
+def runningTimer():
+    global startTime,minutes,seconds,timerState
+
+    countDownTimeS=(minutes*60)+seconds
+
+    minutes=int(countDownTimeS//60)
+    seconds=int(countDownTimeS%60)
+    timeStr="{:02d}:{:02d}".format(minutes,seconds)
+    showTime(timeStr,0)
+    time.sleep(1)
+    startTime=time.monotonic()
+    while(True):
+        currentTime=time.monotonic()
+        timeRemaining=countDownTimeS-(currentTime-startTime)
+
+        # If time is up, display the blastoff text
+        if(timeRemaining<=0):
+            displayBlastoffText()
+            break
+        
+        minutes=int(timeRemaining//60)
+        seconds=int(timeRemaining%60)
+        timeStr="{:02d}:{:02d}".format(minutes,seconds)
+
+        # Prints remaining time
+        showTime(timeStr,0)
+
+        # If the up button is pressed, pause the timer
+        if(not upButton.value):
+            time.sleep(0.3)
+            timerState=TimerState.PAUSED
+            break
+        # If the down button is pressed, reset the timer
+        elif(not downButton.value):
+            time.sleep(0.3)
+            timerState=TimerState.SETTING
+            minutes=0
+            seconds=0
+            break
+
+# Handles paused state
+def pausedTimer():
+    global minutes,seconds,timerState
+
+    timeStr="{:02d}:{:02d}".format(minutes,seconds)
+
+    # Prints the paused time
+    showTime(timeStr,2)
+
+    while(True):
+        # If the up button is pressed, resume the timer
+        if(not upButton.value):
+            time.sleep(0.3)
+            timerState=TimerState.RUNNING
+            break
+        # If the down button is pressed, reset the timer
+        elif(not downButton.value):
+            time.sleep(0.3)
+            timerState=TimerState.SETTING
+            minutes=0
+            seconds=0
+            break
+
+# Displays the blastoff text
+def displayBlastoffText():
+    global timerState,minutes,seconds
+
+    matrixPortal.display.show(blastOffTextArea) # Displays the blastoff text
+    hue=0
+    while(True):
+        # Adjust the x-coordinate for smooth scrolling
+        blastOffTextArea.x-=1
+
+        # Increment the hue value for the color of the text
+        hue=(hue+1)%1536
+        blastOffTextArea.color=rainbowio.colorwheel(hue)
+
+        # If the right edge of the label is off the left side of the screen, move it back to the right
+        if(blastOffTextArea.x+blastOffTextArea.width<-70):
+            blastOffTextArea.x=matrixPortal.display.width
+
+        # Update the display
+        matrixPortal.display.refresh()
+
+        # Add a small delay to control the scrolling speed
+        time.sleep(0.02)
+
+        # If the up button is pressed, reset the timer
+        if(not downButton.value):
+            time.sleep(0.3)
+            timerState=TimerState.SETTING
+            minutes=0
+            seconds=0
+            break
+
+# Displays the time
+def showTime(time,mode):
+    if(mode==0):
+        setColor=0xFF0000 # Red 
+    elif(mode==1):
+        setColor=0x00FF00 # Green
+    elif(mode==2):
+        setColor=0x0000FF # Blue
+
+    textArea=bitmap_label.Label(terminalio.FONT,text=time,scale=2,x=3,y=16,color=setColor)
+    matrixPortal.display.show(textArea)
+
+# Initialize the matrix portal
+matrixPortal=MatrixPortal(status_neopixel=board.NEOPIXEL)
+
+# Initialize the blastoff text
+blastOffTextArea=bitmap_label.Label(terminalio.FONT,text="BLASTOFF!!!",scale=2,x=matrixPortal.display.width,y=16,color=0xFF0000)
+
+# Initialize the buttons
+upButtonPin=board.BUTTON_UP
+downButtonPin=board.BUTTON_DOWN
+
+upButton=digitalio.DigitalInOut(upButtonPin)
+upButton.switch_to_input(pull=digitalio.Pull.UP)
+
+downButton=digitalio.DigitalInOut(downButtonPin)
+downButton.switch_to_input(pull=digitalio.Pull.UP)
 
 # States for the timer
 class TimerState:
@@ -47,141 +175,11 @@ class TimerState:
     PAUSED=2
 
 startTime=0  # Start time when the timer begins
-countDownTime=0  # Countdown time in MM:SS
+minutes=0 # Minutes for the timer
+seconds=0 # Seconds for the timer
 
 timerState=TimerState.SETTING  # Initialize the timer state to SETTING
-
-def settingTimer():
-    global startTime,countDownTime,timerState
-
-    while(True):
-        minutes=countDownTime//100
-        seconds=countDownTime%100
-        timeStr="{:02d}:{:02d}".format(minutes,seconds)
-
-        # Prints the desired time
-        matrix.display.fill(0)
-        matrix.display.text(timeStr,x=3,y=9,color=0xFFFFFF)
-        matrix.display.show()
-
-        key=keypad.read()
-        if(key is not None):
-            # Checks if 'A' is pressed to start the timer
-            if(key=='A' and countDownTime>0):
-                timerState=TimerState.RUNNING
-                startTime=time.monotonic()
-                break
-            # Checks if 'C' is pressed to reset the timer
-            elif(key=='C'):
-                countDownTime=0
-            # Checks if 'D' is pressed to backspace
-            elif(key=='D' and countDownTime>0):
-                countDownTime//=10
-            # Checks if a number is pressed
-            elif('0'<=key<='9' and countDownTime<=999):
-                countDownTime=countDownTime*10+int(key)
-
-def runningTimer():
-    global countDownTime,timerState
-
-    minutes=countDownTime//100
-    seconds=countDownTime%100
-    countDownTimeS=(minutes*60)+seconds
-
-    while(True):
-        # Calculates the remaining time on the timer
-        currentTime=time.monotonic()
-        timeRemaining=countDownTimeS-currentTime-startTime
-
-        if(timeRemaining>6039000):
-            displayBlastoffText()
-            break
-
-        minutes=timeRemaining//60
-        seconds=timeRemaining%60
-        timeStr="{:02d}:{:02d}".format(minutes,seconds)
-
-        # Prints remaining time
-        matrix.display.fill(0)
-        matrix.display.text(timeStr,x=3,y=9,color=0xFFFFFF)
-        matrix.display.show()
-
-        key=keypad.read()
-        if(key is not None):
-            # Checks if 'B' is pressed to pause the timer
-            if(key=='B'):
-                timerState=TimerState.PAUSED
-                countDownTime=minutes*100+seconds
-                break
-            # Checks if 'C' is pressed to reset the timer
-            elif(key=='C'):
-                timerState=TimerState.SETTING
-                startTime=0
-                countDownTime=0
-                break
-
-def pausedTimer():
-    global startTime,timerState
-
-    minutes=countDownTime//100
-    seconds=countDownTime%100
-    timeStr="{:02d}:{:02d}".format(minutes,seconds)
-
-    # Prints the paused time
-    matrix.display.fill(0)
-    matrix.display.text(timeStr,x=3,y=9,color=0xFFFFFF)
-    matrix.display.show()
-
-    while(True):
-        key=keypad.read()
-        if(key is not None):
-            # Checks if 'A' is pressed to resume the timer
-            if(key=='A'):
-                timerState=TimerState.RUNNING
-                startTime=time.monotonic()
-                break
-            # Checks if 'C' is pressed to reset the timer
-            elif(key=='C'):
-                timerState=TimerState.SETTING
-                startTime=0
-                countDownTime=0
-                break
-
-def displayBlastoffText():
-    global timerState,startTime,countDownTime,hue
-
-    while(True):
-        # Prints "BLASTOFF"
-        matrix.display.fill(0)
-        matrix.display.text(str,x=textX,y=9,color=matrix.display.color565(hue,255,255))
-        matrix.display.show()
-
-        # Moves text left with text wrapping
-        textX-=1
-        if textX<textMin:
-            textX=matrix.display.width
-
-        # Increase hue
-        hue+=7
-        if hue>=1536:
-            hue=0
-
-        key=keypad.read()
-        if(key=='C'):
-            timerState=TimerState.SETTING
-            startTime=0
-            countDownTime=0
-            hue=0
-            break
-
-def setup():
-    matrix.display.auto_refresh=False
-    matrix.display.show()
-    matrix.display.auto_refresh=True
-
-def loop():
-    global timerState
-
+while(True):
     # Handles setting the timer
     if(timerState==TimerState.SETTING):
         settingTimer()
